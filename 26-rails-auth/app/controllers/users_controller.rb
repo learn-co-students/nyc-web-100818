99321f862b73ec02781d384feb0d10c88db60158
andrew cohn <andrewcohn0@gmail.com>
2018-11-13 11:07:09 -0500

    AUTH AUTH AUTH AUTH AUTH AUTH AUTH

---
+++ 26-rails-auth/app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+  before_action :find_user, only: [:show]
+  def show
+    render :show
+  end
+
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create #POST request to /users FOR SIGNUP
+    @user = User.create(user_params) # User.create is a method on our MODEL which will encrypt and store the user's plaintext password
+    redirect_to @user
+  end
+
+  private
+
+  def find_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+end
---
+++ 26-rails-auth/app/models/user.rb
@@ -0,0 +1,30 @@
+class User < ApplicationRecord
+  has_secure_password # RAILS MAGIC WOW OMG WUT (comes from ActiveModel)
+  #as a reminder, ActiveRecord provides getters and setters for the attributes specified by our schema
+  # therefore, we have getters/setters for username and password_digest
+
+  # user provides their password via some form
+  # run validations (this happens automagically)
+  #
+  # def password=(plaintext_pw)
+  #   # plaintext_pw -> 'michael' for example
+  #   # take the user's plaintext password and encrypt it
+  #   # store the salted and hashed password in our database
+  #   # self is the newly created user
+  #   self.password_digest = BCrypt::Password.create(plaintext_pw)
+  # end
+  #
+  # def authenticate(plaintext_pw) #is the user who they claim to be
+  #   # this is an instance method on an ALREADY EXISTING USER
+  #   # plaintext_pw -> 'flavortown', for example
+  #   # if the provided plaintext_pw matches our salted and hashed pw when run through bcrypt
+  #   # then the user is who they claim they are!
+  #   # same method: is_password? && BCrypt::Password#==
+  #   # BCrypt::Password.new(self.password_digest).is_password?(plaintext_pw)
+  #   if BCrypt::Password.new(self.password_digest) == plaintext_pw
+  #     self #return the user instance
+  #   else
+  #     false # wrong password! u r not authenticated
+  #   end
+  # end
+end
